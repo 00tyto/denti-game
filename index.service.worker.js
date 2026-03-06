1d09df3c0b3d8071ab9b303a8e56c4633bd96a48
00tyto <bepsca@gmail.com>
2026-03-06 20:00:24 +0100

    intro music

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1772823436|13647711';
+const CACHE_VERSION = '1772823597|174293445';
 /** @type {string} */
 const CACHE_PREFIX = 'DENTI_GAME 2 OK!-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
