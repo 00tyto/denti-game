4ca9cc692277b55dd7e6a61deab06aebf70a048f
00tyto <bepsca@gmail.com>
2026-03-08 01:56:05 +0100

    back to beam without GPUParticles

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1772930368|11699961161';
+const CACHE_VERSION = '1772931160|12492391319';
 /** @type {string} */
 const CACHE_PREFIX = 'DENTI_GAME 2 OK!-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
