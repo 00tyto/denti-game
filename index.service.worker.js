109ec4a31ec46b4d6caf558ac133aad16475c16a
00tyto <bepsca@gmail.com>
2026-03-10 04:15:28 +0100

    new sprites

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1773058099|3978457969';
+const CACHE_VERSION = '1773112440|20192152731';
 /** @type {string} */
 const CACHE_PREFIX = 'DENTI_GAME 2 OK!-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
