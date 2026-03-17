6d0b1dad6db748b383fb25a58beae4e0e70291af
00tyto <bepsca@gmail.com>
2026-03-17 13:55:45 +0100

    crepa tile boss arena

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1773751444|5224472696';
+const CACHE_VERSION = '1773752040|14052058';
 /** @type {string} */
 const CACHE_PREFIX = 'DENTI_GAME 2 OK!-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
