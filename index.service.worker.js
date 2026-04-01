083ced0287e3fb5b0e9344f0e4d789fd5e8afb0c
00tyto <bepsca@gmail.com>
2026-04-01 18:39:21 +0200

    minor adjustments

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1775059167|939047252';
+const CACHE_VERSION = '1775061432|1484114455';
 /** @type {string} */
 const CACHE_PREFIX = 'DENTI_GAME 2 OK!-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
