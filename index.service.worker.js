85532150ad40bcf6f6d14572c35b4af5be0f36c5
00tyto <bepsca@gmail.com>
2026-04-01 18:01:37 +0200

    more sprites polish

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1775036656|6249541384';
+const CACHE_VERSION = '1775059167|939047252';
 /** @type {string} */
 const CACHE_PREFIX = 'DENTI_GAME 2 OK!-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
