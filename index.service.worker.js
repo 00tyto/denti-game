6cacbc0c50fa9fa9a418ebcbaf77ae0de33b8eeb
00tyto <bepsca@gmail.com>
2026-04-15 15:37:12 +0200

    re-export with correct title

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,9 +4,9 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1775832017|120942836';
+const CACHE_VERSION = '1776260094|120597774';
 /** @type {string} */
-const CACHE_PREFIX = 'DENTI_GAME 2 OK!-sw-cache-';
+const CACHE_PREFIX = 'DENTI - a song +-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 /** @type {string} */
 const OFFLINE_URL = 'index.offline.html';
