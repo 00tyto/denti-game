fd41a8a6484cbfb73f474f6bee36ff04b1f6ed22
00tyto <bepsca@gmail.com>
2026-04-15 15:50:30 +0200

    icon index updated

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1776260094|120597774';
+const CACHE_VERSION = '1776260867|100399864';
 /** @type {string} */
 const CACHE_PREFIX = 'DENTI - a song +-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
