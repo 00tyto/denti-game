1e4d72eb9ceae3862cc8b035870f4352dc5543c0
00tyto <bepsca@gmail.com>
2026-05-04 14:42:11 +0200

    removed debug shortcuts

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1776260867|100399864';
+const CACHE_VERSION = '1777898412|444153547';
 /** @type {string} */
 const CACHE_PREFIX = 'DENTI - a song +-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
